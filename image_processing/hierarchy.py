92b9e185a19da0721dd14f066670a37129c9d987
GP\Piet.Goempel <p.goempel@gp-joule.de>
2019-04-28 17:03:18 +0200

    .

--- image_processing/hierarchy.py
+++ image_processing/hierarchy.py
@@ -11,29 +11,36 @@ edged_img = cv2.Canny(blurred_img, 50, 250)
 contours, hierarchy = cv2.findContours(edged_img, cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 
 def show_images():
-    cv2.imshow("normal_img", normal_img)
-    cv2.waitKey(0)
-    cv2.imshow("edged_img", edged_img)
-    cv2.waitKey(0)
+        cv2.imshow("normal_img", normal_img)
+        cv2.waitKey(0)
+        cv2.imshow("edged_img", edged_img)
+        cv2.waitKey(0)
 
 def show_contours_onebyone():
-    i = 0
-    for contour in contours:
-        cv2.drawContours(normal_img, [contour], -1, (0, 255, 0), 2)
-        print("contour number:", i)
-        print(hierarchy[0][i])
-        cv2.imshow("Contour image", normal_img)
-        cv2.waitKey(0)
-        i = i + 1
+        i = 0
+        for contour in contours:
+                cv2.drawContours(normal_img, [contour], -1, (0, 255, 0), 2)
+                print("contour number:", i)
+                print(hierarchy[0][i])
+                cv2.imshow("Contour image", normal_img)
+                cv2.waitKey(0)
+                i = i + 1
 
 def find_board_contour(contours):
-    for contour in contours:
-        if (cv2.arcLength(contour, True) > 2200):
-            print("Contour found. Contour is", contour)
-            cv2.drawContours(normal_img, [contour], -1, (0, 255, 0), 2)
+        board_contours = []
+        i = 0
+        print(len(contours))
+        for contour in contours:
+                i = i+1
+                print(i)
+                if (cv2.arcLength(contour, True) > 200):
+                        print("Contour found. Contour is", contour)
+                        cv2.drawContours(normal_img, [contour], -1, (0, 255, 0), 2)
+                board_contours.append(contour)
+        
+        return board_contours
 
-
+print("Amount contours: " ,len(contours))
 find_board_contour(contours)
-
-cv2.imshow("contours", normal_img)
-cv2.waitKey(0)
+print("Done")
+cv2.destroyAllWindows()
