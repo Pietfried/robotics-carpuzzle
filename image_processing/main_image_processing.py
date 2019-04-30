70bfc9dc50ab3256b8ba6a8bcad3f5804ae9b45b
GP\Piet.Goempel <p.goempel@gp-joule.de>
2019-04-30 13:20:02 +0200

    changed branch.

--- image_processing/main_image_processing.py
+++ image_processing/main_image_processing.py
@@ -6,9 +6,13 @@ import imutils
 normal_img = cv2.imread('images/image3.jpg')
 gray_img = cv2.cvtColor(normal_img, cv2.COLOR_BGR2GRAY)
 blurred_img = cv2.medianBlur(gray_img, 5)
-edged_img = cv2.Canny(blurred_img, 75, 300)
+edged_img = cv2.Canny(blurred_img, 60, 180) # these parameters are important. The image detection behaves differently when changing the contrast.
 
 contours, hierarchy = cv2.findContours(edged_img, cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
+
+def show(img):
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
 
 def show_images():
         cv2.imshow("normal_img", normal_img)
@@ -58,7 +62,8 @@ def find_board_contour_idx(contours):
     idx = 0
     save = 0
     for contour in contours:
-        if (cv2.arcLength(contour, True) > 2100):
+        if (cv2.arcLength(contour, True) > 2000):
+            print("found")
             save = idx
         idx = idx + 1
     return save
@@ -96,6 +101,14 @@ def get_board_area(contour):
 
     return cv2.contourArea(contour)
 
+def get_cut_contour(contour):
+    img = cv2.imread("zebra.jpg")
+    stencil = np.zeros(edged_img.shape).astype(edged_img.dtype)
+    color = [255, 255, 255]
+    cv2.fillPoly(stencil, [contour], color)
+    result = cv2.bitwise_and(edged_img, stencil)
+    return result
+
 def get_piece_contours(img):
     piece_contours = []
     all_contours = get_contours_external(img)
@@ -108,6 +121,7 @@ def get_piece_contours(img):
 def get_slot_contours(img):
     slot_contours = []
     cut_board_img = get_cut_board_img(img, get_contours_ccomp(edged_img))
+    show(cut_board_img)
     contours = get_contours_external(cut_board_img)
     for contour in contours:
         if (cv2.contourArea(contour) > 10000 and cv2.contourArea(contour) > 0):
@@ -133,10 +147,8 @@ def show_matches(matches):
         img = normal_img.copy()
         slot_contour = matches[i][0]
         piece_contour = matches[i][1]
-        center = find_center(piece_contour)
         cv2.drawContours(img, [slot_contour], -1, (0, 255, 0), 2)
         cv2.drawContours(img, [piece_contour], -1, (0, 255, 0), 2)
-        cv2.circle(img, (center[0], center[1]), 7, (255, 255, 255), -1)
         cv2.imshow("Matches", img)
         cv2.waitKey(0)
 
@@ -150,7 +162,14 @@ def find_center(contour):
 slot_contours = get_slot_contours(edged_img)
 piece_contours = get_piece_contours(edged_img)
 
+print(len(piece_contours))
+
+cut_contour_img = get_cut_contour(piece_contours[0])
+show(cut_contour_img)
+
 matches = find_matchtes(slot_contours, piece_contours)
+
+print(find_center(slot_contours[0]))
 
 show_matches(matches)
 
